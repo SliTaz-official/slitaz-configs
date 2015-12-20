33d41b0fa25962adf6c0e0d73e0b23f66d2708e4
Aleksej Bobylev <al.bobylev@gmail.com>
2015-12-21 00:57:00 +0200

    Update webhome (completely authored by Leonardo Laporte); translate to Russian

---
+++ rootfs/usr/share/webhome/app/lib/app.js
@@ -0,0 +1,11 @@
+// Slitaz GNU/Linux : Start Page
+
+// Open | Close Object
+function openclose(obj){
+  var btn = document.getElementById(obj);
+    if(btn.style.display != "block"){
+    btn.style.display = "block";
+   }else{
+     btn.style.display = "none";
+   }
+}
